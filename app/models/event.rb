df5497643422c02bec854ddd67c478445fe4d14c
bossy <bossy@Ruby-Env.myguest.virtualbox.org>
2023-08-10 16:31:42 -0700

    Add class methods for events models for past, current and future events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,16 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
     has_many :event_attendees, foreign_key: :attended_event_id
     has_many :attendees, through: :event_attendees, source: :attendee
+
+    def self.past
+        where("event_date < ?", Date.today)
+    end
+
+    def self.future
+        where("event_date > ?", Date.today)
+    end
+
+    def self.current
+        where("event_date = ?", Date.today)
+    end
 end
